f6e131d703f82b53693956cfc544b5d9eee1290f
Krzysztof Jankowski <w84death@gmail.com>
2018-04-23 22:23:49 +0200

    v0.5

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,37 +1,28 @@
-extends KinematicBody
+extends RigidBody
 
 var trusters = Vector3(0,0,0)
-var speed = 24
-var MOUSESPEED = 0.005
+var speed = 600
+
+export var player_id = 0
+var left_button = [KEY_LEFT, KEY_A]
+var right_button = [KEY_RIGHT, KEY_D]
+var jump_button = [KEY_UP, KEY_W]
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	set_process_input(true)
 	
 func _process(delta):
-
-	if Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_UP) or Input.is_mouse_button_pressed(1):
-		translate(Vector3(0, 0, speed*delta))
-	if Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN) or Input.is_mouse_button_pressed(2):
-		translate(Vector3(0, 0, -speed*delta))
-	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
-		translate(Vector3(speed*delta, 0, 0))
-	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
-		translate(Vector3(-speed*delta, 0, 0))
-
-	if Input.is_key_pressed(KEY_E):
-		translate(Vector3(0, speed*delta, 0))
-	if Input.is_key_pressed(KEY_Q):
-		translate(Vector3(0, -speed*delta, 0))
+	if Input.is_key_pressed(jump_button[player_id]):
+		set_axis_velocity(Vector3(0, speed*2*delta, 0 ))
 		
-func activate_player():
-	get_node("eyes").make_current()
-
+	if Input.is_key_pressed(left_button[player_id]):
+		apply_impulse(Vector3(0,0,0), Vector3(speed*delta, 0, 0))
+	if Input.is_key_pressed(right_button[player_id]):
+		apply_impulse(Vector3(0,0,0), Vector3(-speed*delta, 0, 0))
+		
+	
+	
 func _input(event):
-	if event is InputEventMouseMotion:
-		rotation.y -= event.relative.x*MOUSESPEED
-		rotation.x = clamp(rotation.x + event.relative.y*MOUSESPEED, deg2rad(-90), deg2rad(90))
-
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
